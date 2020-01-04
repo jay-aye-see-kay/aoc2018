ba1eb2b813fc291e54ae29fa34c630cd0d091e0e
Jack Rose <jackderryrose@gmail.com>
2020-01-04 14:03:49 +1100

    clean up

--- 03/main.py
+++ 03/main.py
@@ -5,25 +5,24 @@ def parse_claim(string):
     id, _, offset, size = string.split(" ")
     x, y = offset[:-1].split(",")
     width, height = size.split("x")
-    return (
-        id[1:],
-        {"x": int(x), "y": int(y), "width": int(width), "height": int(height)},
-    )
+    x_range = range(int(x), int(x) + int(width))
+    y_range = range(int(y), int(y) + int(height))
+    coord_list = []
+    for x in x_range:
+        for y in y_range:
+            coord_list.append((x, y))
+    return id[1:], coord_list
 
 
 claim_list = [parse_claim(claim_string) for claim_string in data]
 claim_map = {}
 
-for id, claim in claim_list:
-    x_range = range(claim["x"], claim["x"] + claim["width"])
-    y_range = range(claim["y"], claim["y"] + claim["height"])
-    for x in x_range:
-        for y in y_range:
-            coord = (x, y)
-            if coord in claim_map:
-                claim_map[coord].append(id)
-            else:
-                claim_map[coord] = [id]
+for id, coord_list in claim_list:
+    for coord in coord_list:
+        if coord in claim_map:
+            claim_map[coord].append(id)
+        else:
+            claim_map[coord] = [id]
 
 overlap_count = 0
 for coord, id_list in claim_map.items():
@@ -33,14 +32,10 @@ for coord, id_list in claim_map.items():
 print("overlap_count", overlap_count)
 
 
-for id, claim in claim_list:
+for id, coord_list in claim_list:
     claim_not_overlapping = True
-    x_range = range(claim["x"], claim["x"] + claim["width"])
-    y_range = range(claim["y"], claim["y"] + claim["height"])
-    for x in x_range:
-        for y in y_range:
-            coord = (x, y)
-            if len(claim_map[coord]) != 1:
-                claim_not_overlapping = False
+    for coord in coord_list:
+        if len(claim_map[coord]) != 1:
+            claim_not_overlapping = False
     if claim_not_overlapping:
-        print('id', id)
+        print("id", id)
