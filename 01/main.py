30113e5283eb14a4ba10f47fd06fb66b1019e86f
Jack Rose <jackderryrose@gmail.com>
2020-01-04 11:35:11 +1100

    1.2 completed

--- 01/main.py
+++ 01/main.py
@@ -1,8 +1,17 @@
 data = open("data.txt")
+freq_change_list = [int(change) for change in data]
 
 freq_value = 0
+freqs_seen = { freq_value: 1 }
+found_duplicate = False
 
-for freq_change in data:
-    freq_value += int(freq_change)
+while not found_duplicate:
+    for freq_change in freq_change_list:
+        freq_value += freq_change
+        if freq_value in freqs_seen:
+            found_duplicate = True
+            break
+        else:
+            freqs_seen[freq_value] = 1
 
 print(freq_value)
