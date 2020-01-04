f76aa37b0abdeda6f5aaa65fa7c1534e562cf5a3
Jack Rose <jackderryrose@gmail.com>
2020-01-04 13:00:55 +1100

    2.2 complete

--- 02/main.py
+++ 02/main.py
@@ -1,31 +1,22 @@
 data = open("data.txt")
-
-letter_count_list = []
-
-for barcode in data:
-    letter_count = {}
-    for letter in barcode:
-        if letter in letter_count:
-            letter_count[letter] += 1
-        else:
-            letter_count[letter] = 1
-    letter_count_list.append(letter_count)
-
-has2 = 0
-has3 = 0
-
-for letter_count in letter_count_list:
-    barcode_has2 = False
-    barcode_has3 = False
-    for v in letter_count.values():
-        if v == 2:
-            barcode_has2 = True
-        if v == 3:
-            barcode_has3 = True
-    if barcode_has2:
-        has2 += 1
-    if barcode_has3:
-        has3 += 1
+barcode_list = [barcode for barcode in data]
 
 
-print(has2 * has3)
+def barcode_match(barcode1, barcode2):
+    diff_count = 0
+    for i, char1 in enumerate(barcode1):
+        char2 = barcode2[i]
+        if char1 != char2:
+            diff_count += 1
+    return diff_count == 1
+
+
+for barcode1 in barcode_list:
+    for barcode2 in barcode_list:
+        if barcode1 == barcode2:
+            continue
+        match = barcode_match(barcode1, barcode2)
+        if match:
+            print("barcode1", barcode1)
+            print("barcode2", barcode2)
+            break
