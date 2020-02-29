d878950372d60b3b501c30ac7833cb3b91bfc9fe
Jack Rose <jackderryrose@gmail.com>
2020-02-29 15:52:38 +1100

    clean up 6.1

--- 06/main.py
+++ 06/main.py
@@ -2,12 +2,12 @@ data = open("data.txt")
 
 max_x = 0
 max_y = 0
-coordinates = []
+coord_details = {}
 for line in data:
     x_str, y_str = line.split(', ')
     x = int(x_str)
     y = int(y_str)
-    coordinates.append((x, y))
+    coord_details[(x, y)] = { "count": 1, "isInf": False }
     if x > max_x:
         max_x = x
     if y > max_y:
@@ -31,31 +31,15 @@ def get_nearest(x, y, coordinates):
     return closest_coord
 
 
-star_map = {}
-coord_details = { coords: { "count": 1, "isInf": False } for coords in coordinates }
-
 for x in range(0, max_x + 1):
     for y in range(0, max_y + 1):
-
-        if (x, y) in coordinates:
-            # when x, y is a coord
-            if not x in star_map.keys():
-                star_map[x] = {}
-            star_map[x][y] = (x, y)
-
-        else:
+        if (x, y) not in coord_details.keys():
             # when x, y is empty space
-            if not x in star_map.keys():
-                star_map[x] = {}
-            nearest_coord = get_nearest(x, y, coordinates)
-            star_map[x][y] = nearest_coord
+            nearest_coord = get_nearest(x, y, coord_details.keys())
             if nearest_coord:
                 coord_details[nearest_coord]["count"] += 1
                 if x == 0 or x == max_x or y == 0 or y == max_y:
                     coord_details[nearest_coord]["isInf"] = True
-
-
-print('coord_details', coord_details)
 
 
 max_valid_count = 0
@@ -64,6 +48,3 @@ for coord, coord_detail in coord_details.items():
         max_valid_count = coord_detail["count"]
 
 print('max_valid_count', max_valid_count)
-
-
-# print('star_map', star_map)
