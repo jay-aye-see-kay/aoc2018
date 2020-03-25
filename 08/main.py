55196c237514cf5e26dac4bad28cd8c088d354c7
Jack Rose <jackderryrose@gmail.com>
2020-03-25 14:11:25 +1100

    8.2 complete

--- 08/main.py
+++ 08/main.py
@@ -14,6 +14,26 @@ def walk(index, tree, count):
         index += 1
     return count, index
 
+    
+def walk_two(index, tree):
+    num_children = tree[index]
+    index+=1
+    num_metadata = tree[index]
+    index+=1
+    child_values = []
+    value = 0
+    for _ in range(0, num_children):
+        child_value, index = walk_two(index, tree)
+        child_values.append(child_value)
+    for _ in range(0, num_metadata):
+        if num_children == 0:
+            value += tree[index]
+        else:
+            current_metadata = tree[index]
+            if current_metadata > 0 and current_metadata <= num_children:
+                value += child_values[current_metadata-1]
+        index += 1
+    return value, index
 
-answer, _ = walk(0, tree, 0)
+answer, _ = walk_two(0, tree)
 print(answer)
