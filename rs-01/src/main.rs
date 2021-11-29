17b83b367587a09f8cd634f2d3cb030ee4096462
Jack Rose <user@jackrose.co.nz>
2021-11-30 07:14:44 +1100

    rust: day 1 part 2

--- rs-01/src/main.rs
+++ rs-01/src/main.rs
@@ -1,8 +1,10 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
     println!("part 1: {}", part_1(&input));
+    println!("part 2: {}", part_2(&input));
 }
 
 /// Starting with a frequency of zero, what is the resulting frequency after all of the changes in
@@ -16,6 +18,24 @@ fn part_1(input: &str) -> i32 {
     return sum;
 }
 
+
+/// find the first frequency it reaches twice
+fn part_2(input: &str) -> i32 {
+    let mut sum = 0;
+    let mut seen = HashSet::new();
+
+    loop {
+        for line in input.lines() {
+            let val = line.parse::<i32>().unwrap();
+            sum += val;
+            if seen.contains(&sum) {
+                return sum;
+            }
+            seen.insert(sum);
+        }
+    }
+}
+
 // tests
 #[cfg(test)]
 mod tests {
@@ -25,5 +45,11 @@ mod tests {
     fn test_part_1() {
         let input = "+1\n-2\n+3\n+1\n";
         assert_eq!(part_1(input), 3);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = "+1\n-2\n+3\n+1\n";
+        assert_eq!(part_2(input), 2);
     }
 }
