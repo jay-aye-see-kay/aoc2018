a7ca6e27c81e72eb69e7c2fb694699677de3c92b
Jack Rose <user@jackrose.co.nz>
2021-11-30 06:59:35 +1100

    rust: day 1 part 1

---
+++ rs-01/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
+    println!("part 1: {}", part_1(&input));
+}
+
+/// Starting with a frequency of zero, what is the resulting frequency after all of the changes in
+/// frequency have been applied?
+fn part_1(input: &str) -> i32 {
+    let mut sum = 0;
+    for line in input.lines() {
+        let val = line.parse::<i32>().unwrap();
+        sum += val;
+    }
+    return sum;
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = "+1\n-2\n+3\n+1\n";
+        assert_eq!(part_1(input), 3);
+    }
+}
